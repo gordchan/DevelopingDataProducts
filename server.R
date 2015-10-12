f113f576ab5fdc06619be8979a90f1d8d7dc0fe6
gordchan <gordon.chan@mensa.org.hk>
2015-10-12 11:33:24 +0000

    Link to sample data

--- server.R
+++ server.R
@@ -6,24 +6,26 @@
 library(shiny)
 library(xlsx)
 
+#test pre-computation
+
+a = c(101:110)
+b = sample(11:20, 10, replace = TRUE)
+c = sample(101:200, 10, replace = TRUE)
+
+df1 <- data.frame("Serial" = a, "Banana" = b)
+df2 <- data.frame("Serial" = a, "Cats" = c)
+
 shinyServer(function(input, output) {
     datasetInput <- reactive({
         switch(input$dataset,
-               "2020-Jan" = rock,
-               "2021-Jan" = pressure)
+               "2020-Jan" = df1,
+               "2021-Jan" = df2)
     })
     
     output$table <- renderTable({
         datasetInput()
     })
     
-    output$downloadData <- downloadHandler(
-        filename = function() { 
-            paste(input$dataset, '.csv', sep='') 
-        },
-        content = function(file) {
-            write.csv(datasetInput(), file)
-        }
-    )
+
 })
 
--- ui.R
+++ ui.R
@@ -7,12 +7,17 @@
 library(shiny)
 
 shinyUI(fluidPage(
-  titlePanel('Downloading Data'),
+  titlePanel('Compilation of KPI report'),
   sidebarLayout(
     sidebarPanel(
-      selectInput("dataset", "Choose a dataset:", 
-                  choices = c("2020-Jan", "2021-Jan")),
-      downloadButton('downloadData', 'Download')
+      # adding the new div tag to the sidebar            
+      tags$div(class="header", checked=NA,
+               tags$strong("1. Download the sample data of A&E waiting time"),
+               tags$a(href="./source/Jan20/kpi.1_AE_WT_2020-01.xlsx", "January 2020"), br(),
+               tags$a(href="./source/Jan21/kpi.1_AE_WT_2021-01.xlsx", "January 2021")), br(), br(),
+      # view sample data
+      selectInput("dataset", "View the sample dataset:", 
+                  choices = c("2020-Jan", "2021-Jan"))
     ),
     mainPanel(
       tableOutput('table')
