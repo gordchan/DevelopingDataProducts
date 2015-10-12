11454a6ba8ac5a9f182764c1ce5ff6b29c3038c7
gordchan <gordon.chan@mensa.org.hk>
2015-10-12 11:33:42 +0000

    Deploy script directly to shiny-server

--- deploy.R
+++ deploy.R
@@ -10,7 +10,7 @@ deployApp <- function(deploy = "app")
         file.copy(from = "ui.R", to = "/home/gord/shiny-server/ShinyKPI/", recursive = TRUE)
         file.copy(from = "server.R", to = "/home/gord/shiny-server/ShinyKPI/", recursive = TRUE)
         # Deploy source data & template to shiny-server
-        file.copy(from = "./source/", to = "/home/gord/shiny-server/ShinyKPI/", recursive = TRUE)
+        file.copy(from = "./www/source/", to = "/home/gord/shiny-server/ShinyKPI/www/", recursive = TRUE)
         file.copy(from = "./template/", to = "/home/gord/shiny-server/ShinyKPI/", recursive = TRUE)
     } else if (deploy=="app"){
         # Deploy app to shiny-server
@@ -18,7 +18,7 @@ deployApp <- function(deploy = "app")
         file.copy(from = "server.R", to = "/home/gord/shiny-server/ShinyKPI/", recursive = TRUE)
     } else if (deploy=="data"){
         # Deploy source data & template to shiny-server
-        file.copy(from = "./source/", to = "/home/gord/shiny-server/ShinyKPI/", recursive = TRUE)
+        file.copy(from = "./www/source/", to = "/home/gord/shiny-server/ShinyKPI/www/", recursive = TRUE)
         file.copy(from = "./template/", to = "/home/gord/shiny-server/ShinyKPI/", recursive = TRUE)
     } else {
         # Show usage
